2fa44e908ad59e4541d6d3445f51f3000a53bfcc
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-19 23:54:32 +0000

    refactor(Success/Failure): change to return `void`.

--- src/lib/failure-callback.type.ts
+++ src/lib/failure-callback.type.ts
@@ -1,5 +1,10 @@
+/**
+ * @description Represents a callback function invoked after a failed action.
+ * @export
+ * @template [Value=unknown] The type of the value associated with the failed action. Defaults to `unknown`.
+ * @template [Payload=unknown] The type of additional data that may be provided. Defaults to `unknown`.
+ */
 export type FailureCallback<
   Value = unknown,
-  Payload = unknown,
-  Return = void
-> = (value: Value, payload?: Payload) => Return;
+  Payload = unknown
+> = (value: Value, payload?: Payload) => void;
