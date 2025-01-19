97f79ce2023d0b3243e4e70da1ebaace44677107
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-19 23:55:06 +0000

    docs: update.

--- src/lib/error-message.type.ts
+++ src/lib/error-message.type.ts
@@ -1,1 +1,7 @@
+/**
+ * @description The type of error message to use with context and payload.
+ * @export
+ * @template Context The generic type value of context in which the error occurred.
+ * @template Payload The generic type value of payload to gives additional data.
+ */
 export type ErrorMessage<Context, Payload> = (context: Context, payload: Payload) => string;
