9f7688654772d64129c3a8d9ef6b0430583d919d
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-19 23:54:08 +0000

    refactor(ValidationCallback): change the returned value to always `boolean`.

--- src/lib/validation-callback.type.ts
+++ src/lib/validation-callback.type.ts
@@ -1,7 +1,12 @@
 // Type.
 import { CommonCallback } from "./common-callback.type";
+/**
+ * @description Represents the type of function that takes the validation result of the `Value` of `boolean` type.
+ * @export
+ * @template [Value=unknown] The type of the value being validated. Defaults to `unknown`.
+ * @template [Payload=unknown] The type of additional data that may be used in post-validation. Defaults to `unknown`.
+ */
 export type ValidationCallback<
   Value = unknown,
   Payload = unknown,
-  Return = boolean
-> = CommonCallback<boolean, Value, Payload, Return>;
+> = CommonCallback<boolean, Value, Payload, boolean>;
