a2e44d2a87af0ba724bb3adee47959f96bb797a3
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-19 23:58:07 +0000

    refactor(ResultCallback): add the `Result` generic type variable, and return it.

--- src/lib/result-callback.type.ts
+++ src/lib/result-callback.type.ts
@@ -1,6 +1,14 @@
 // Type.
 import { CommonCallback } from "./common-callback.type";
+/**
+ * @description Represents the type of function that takes the result of the processed value and return it.
+ * @export
+ * @template [Result=any] The type of the result. Defaults to, `any`.
+ * @template [Value=unknown] The type of the processed value. Defaults to, `unknown`.
+ * @template [Payload=unknown] The type of additional data that may be provided. Defaults to `unknown`.
+ */
 export type ResultCallback<
+  Result = any,
   Value = unknown,
   Payload = unknown,
-> = CommonCallback<any, Value, Payload, Value>;
+> = CommonCallback<Result, Value, Payload, Result>;
