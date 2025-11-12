cd6d67044532edb62754121f74e307df1edc6b61
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-11-12 00:25:03 +0000

    chore(api): update.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,12 +1,20 @@
-export type { AsyncCallback } from './async-callback.type';
-export type { CommonCallback } from './common-callback.type';
-export type { ErrorCallback } from './error-callback.type';
-export type { ErrorMessage } from './error-message.type';
-export type { FailureCallback } from './failure-callback.type';
-export type { GetterCallback } from './getter-callback.type';
-export type { ProcessCallback } from './process-callback.type';
-export type { ResultCallback } from './result-callback.type';
-export type { SetterCallback } from './setter-callback.type';
-export type { StatusCallback } from './status-callback.type';
-export type { SuccessCallback } from './success-callback.type';
-export type { ValidationCallback } from './validation-callback.type';
+export type { AsyncCallback } from './async-callback.interface';
+export type { CommonCallback } from './common-callback.interface';
+export type { ErrorCallback } from './error-callback.interface';
+export type { FailureCallback } from './failure-callback.interface';
+export type { GetterCallback } from './getter-callback.interface';
+
+export type { OnChangeCallback } from './on-change-callback.interface';
+export type { OnDestroyCallback } from './on-destroy-callback.interface';
+export type { OnPropertyChangeCallback } from './on-property-change-callback.interface';
+export type { OnSetCallback } from './on-set-callback.interface';
+export type { OnSetPropertyCallback } from './on-set-property-callback.interface';
+
+export type { ProcessCallback } from './process-callback.interface';
+export type { ResultCallback } from './result-callback.interface';
+export type { SetterCallback } from './setter-callback.interface';
+export type { StatusCallback } from './status-callback.interface';
+export type { SuccessCallback } from './success-callback.interface';
+export type { ValidationCallback } from './validation-callback.interface';
+
+export type { ErrorMessage } from './error-message.interface';
